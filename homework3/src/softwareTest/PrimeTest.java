2b6a4a2057f5b0fb6af01b221ebc806bb8ce4142
maomaoL <13102215515@163.com>
2019-03-23 11:27:40 +0800

    homework3

---
+++ homework3/src/softwareTest/PrimeTest.java
@@ -0,0 +1,19 @@
+package softwareTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrimeTest {
+	@Test
+	public void test1() {
+		PrintPrimes.printPrimes(1);
+	}
+//	@Test
+//	public void test2() {
+//		System.out.println("--------------------------");
+//		PrintPrimes.printPrimes(1);
+//	}
+}
